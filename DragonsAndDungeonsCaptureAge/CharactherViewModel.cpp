9f423556768c0290d711d787edb3bb2bf37b79c2
devoecht <jana.devoecht@gmail.com>
2024-08-27 12:02:03 +0200

    Adding first logic to the CharacterViewModel

--- DragonsAndDungeonsCaptureAge/CharactherViewModel.cpp
+++ DragonsAndDungeonsCaptureAge/CharactherViewModel.cpp
@@ -6,13 +6,134 @@
 
 namespace winrt::DragonsAndDungeonsCaptureAge::implementation
 {
-    int32_t CharactherViewModel::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
+	//Name
+	winrt::hstring CharactherViewModel::CharacterName() const
+	{
+		return m_characterName;
+	}
+	void CharactherViewModel::CharacterName(winrt::hstring value)
+	{
+		if (m_characterName != value)
+		{
+			m_characterName = value;
+		}
+	}
 
-    void CharactherViewModel::MyProperty(int32_t /*value*/)
-    {
-        throw hresult_not_implemented();
-    }
+	//Race
+	CharacterRace CharactherViewModel::CharacterRaceID() const
+	{
+		return m_characterRace;
+	}
+	void CharactherViewModel::CharacterRaceID(CharacterRace value)
+	{
+		if (m_characterRace != value)
+		{
+			m_characterRace = value;
+		}
+	}
+
+	//class
+	CharacterClass CharactherViewModel::CharacterClassID() const
+	{
+		return m_characterClass;
+	}
+	void CharactherViewModel::CharacterClassID(CharacterClass value)
+	{
+		if (m_characterClass != value)
+		{
+			m_characterClass = value;
+		}
+	}
+
+	//Prime stat
+	CharacterStatType CharactherViewModel::CharacterPrimeClassID() const
+	{
+		return m_characterPrimeStat;
+	}
+	void CharactherViewModel::CharacterPrimeClassID(CharacterStatType value)
+	{
+		if (m_characterPrimeStat != value)
+		{
+			m_characterPrimeStat = value;
+		}
+	}
+
+	//Stat values
+	int16_t CharactherViewModel::CharacterSTRStatValue() const
+	{
+		return m_CharacterSTRStatValue;
+	}
+
+	void CharactherViewModel::CharacterSTRStatValue(int16_t value)
+	{
+		if (m_CharacterSTRStatValue != value)
+		{
+			m_CharacterSTRStatValue = value;
+		}
+	}
+
+	int16_t CharactherViewModel::CharacterINTStatValue() const
+	{
+		return m_CharacterINTStatValue;
+	}
+
+	void CharactherViewModel::CharacterINTStatValue(int16_t value)
+	{
+		if (m_CharacterINTStatValue != value)
+		{
+			m_CharacterINTStatValue = value;
+		}
+	}
+
+	int16_t CharactherViewModel::CharacterWISStatValue() const
+	{
+		return m_CharacterWISStatValue;
+	}
+
+	void CharactherViewModel::CharacterWISStatValue(int16_t value)
+	{
+		if (m_CharacterWISStatValue != value)
+		{
+			m_CharacterWISStatValue = value;
+		}
+	}
+
+	int16_t CharactherViewModel::CharacterDEXStatValue() const
+	{
+		return m_CharacterDEXStatValue;
+	}
+
+	void CharactherViewModel::CharacterDEXStatValue(int16_t value)
+	{
+		if (m_CharacterDEXStatValue != value)
+		{
+			m_CharacterDEXStatValue = value;
+		}
+	}
+
+	int16_t CharactherViewModel::CharacterCONStatValue() const
+	{
+		return m_CharacterCONStatValue;
+	}
+
+	void CharactherViewModel::CharacterCONStatValue(int16_t value)
+	{
+		if (m_CharacterCONStatValue != value)
+		{
+			m_CharacterCONStatValue = value;
+		}
+	}
+
+	int16_t CharactherViewModel::CharacterCHAStatValue() const
+	{
+		return m_CharacterCHAStatValue;
+	}
+
+	void CharactherViewModel::CharacterCHAStatValue(int16_t value)
+	{
+		if (m_CharacterCHAStatValue != value)
+		{
+			m_CharacterCHAStatValue = value;
+		}
+	}
 }
--- DragonsAndDungeonsCaptureAge/CharactherViewModel.h
+++ DragonsAndDungeonsCaptureAge/CharactherViewModel.h
@@ -8,8 +8,48 @@ namespace winrt::DragonsAndDungeonsCaptureAge::implementation
     {
         CharactherViewModel() = default;
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
+        winrt::hstring CharacterName() const;
+        void CharacterName(winrt::hstring value);
+
+        CharacterRace CharacterRaceID() const;
+        void CharacterRaceID(CharacterRace value);
+
+        CharacterClass CharacterClassID() const;
+        void CharacterClassID(CharacterClass value);
+
+        CharacterStatType CharacterPrimeClassID() const;
+        void CharacterPrimeClassID(CharacterStatType value);
+
+        int16_t CharacterSTRStatValue() const;
+        void CharacterSTRStatValue(int16_t value);
+
+        int16_t CharacterINTStatValue() const;
+        void CharacterINTStatValue(int16_t value);
+
+        int16_t CharacterWISStatValue() const;
+        void CharacterWISStatValue(int16_t value);
+
+        int16_t CharacterDEXStatValue() const;
+        void CharacterDEXStatValue(int16_t value);
+
+        int16_t CharacterCONStatValue() const;
+        void CharacterCONStatValue(int16_t value);
+
+        int16_t CharacterCHAStatValue() const;
+        void CharacterCHAStatValue(int16_t value);
+
+    private:
+        winrt::hstring m_characterName = L"Default Name";
+        CharacterRace m_characterRace = CharacterRace::Unknown;
+        CharacterClass m_characterClass = CharacterClass::Unknown;
+        CharacterStatType m_characterPrimeStat = CharacterStatType::Unknown;
+
+        int16_t m_CharacterSTRStatValue = 0;
+        int16_t m_CharacterINTStatValue = 0;
+        int16_t m_CharacterWISStatValue = 0;
+        int16_t m_CharacterDEXStatValue = 0;
+        int16_t m_CharacterCONStatValue = 0;
+        int16_t m_CharacterCHAStatValue = 0;
     };
 }
 
